3ce48a38b8ca36ccb494bee566a227099fd25c88
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2024-09-23 08:41:19 +0700

    authentication JWT, Bcrypt, exceiption

---
+++ src/main/java/net/javaspringboot/kpis_be01/dto/response/ApiResponse.java
@@ -0,0 +1,19 @@
+package net.javaspringboot.kpis_be01.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // Chỉ hiện các field không null
+public class ApiResponse<T> {
+    private int code = 1000;  // Mã phản hồi mặc định
+    private String message;   // Thông điệp phản hồi
+    private T result;         // Dữ liệu kết quả trả về
+
+
+}
---
+++ src/main/java/net/javaspringboot/kpis_be01/dto/response/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package net.javaspringboot.kpis_be01.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthenticationResponse {
+    String token;
+    boolean authenticated;
+}
